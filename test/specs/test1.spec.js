b9dc471bf2a5be60f6cbf52b688ea4babf1d8377
roonicz <roonicz@gmail.com>
2024-10-10 08:00:16 +0530

    Commit V2

--- test/specs/test1.spec.js
+++ test/specs/test1.spec.js
@@ -6,7 +6,7 @@ describe('Demo Test', function () {
         await browser.url('https://www.google.com')
         await $('[name="q"]').setValue("Try Wdio")
         browser.keys('Enter')
-        browser.pause(5000)
+        browser.pause(5500)
         //await $('[name="btnK"]').click()
 
 
